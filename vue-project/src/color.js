27fd8b5b85b63eca03255ad79f5adf1f9214195f
IRY21 <rilok21work@gmail.com>
2018-10-22 15:04:02 +0500

    modifiers

--- vue-project/src/color.js
+++ vue-project/src/color.js
@@ -1,14 +1,28 @@
 export default {
-    bind(el, bindings, vnode) {
-        /* console.log('bind');
+  bind(el, bindings, vnode) {
+    /* console.log('bind');
         el.style.color = 'blue'; */
 
-        const arg = bindings.arg;
 
-        el.style[arg] = bindings.value;
+    const fontModifier = bindings.modifiers["font"];
+    if (fontModifier) {
+      el.style.fontSize = "30px";
     }
 
-    /* inserted(el, bindings, vnode) {
+    const delayModifier = bindings.modifiers["delay"];
+    
+    let delay = 0;
+    
+    if (delayModifier) {
+        delay = 2000;
+    }
+    setTimeout(() => {
+        const arg = bindings.arg;
+        el.style[arg] = bindings.value;
+    }, delay);
+  }
+
+  /* inserted(el, bindings, vnode) {
         console.log('inserted');
     },
 
