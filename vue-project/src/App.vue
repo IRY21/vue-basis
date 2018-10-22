94a3dd76a524a0540f00649c7250ce107fe42128
IRY21 <rilok21work@gmail.com>
2018-10-22 12:19:23 +0500

    registration-component.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,13 +1,23 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <h1>{{ msg }}</h1>
+
+    <appCar></appCar>
+  </div>
 </template>
 
 <script>
+
+import Car from './Car.vue'
+
 export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components: {
+    appCar: Car
   }
 }
 </script>
