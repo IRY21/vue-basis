839b719c106026d3b0121c867c123398bc31e710
IRY21 <rilok21work@gmail.com>
2018-10-22 14:33:41 +0500

    directive lifecycle

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-      <h2 v-colored>{{ title }}</h2>
+      <h2 v-colored v-if="visible">{{ title }}</h2>
+
+      <button @click="visible = !visible">Visibility toggle</button>
+      <button @click="title = 'New title'">Change title</button>
   </div>
 </template>
 
@@ -8,7 +11,8 @@
 export default {
   data() {
     return {
-      title: "Hello I am Vue!"
+      title: "Hello I am Vue!",
+      visible: true
     };
   }
 };
