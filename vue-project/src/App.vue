91074703e8833ab48732225273ca2ae1a63461d0
IRY21 <rilok21work@gmail.com>
2018-10-22 14:22:58 +0500

    own directive

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,30 +1,19 @@
 <template>
   <div>
-    <appCar>
-      <h2 slot="title">{{ carName }} + html</h2>
-      <p slot="text">Lorem, ipsum dolor.</p>
-    </appCar>
+      <h2>{{ title }}</h2>
   </div>
 </template>
 
 <script>
-import Car from "./Car.vue";
-
 export default {
   data() {
     return {
-      carName: "Tesla"
+      title: "Hello I am Vue!"
     };
-  },
-
-  components: {
-    appCar: Car
   }
 };
 </script>
 
 <style scoped>
-h2 {
-  color: red;
-}
+
 </style>
